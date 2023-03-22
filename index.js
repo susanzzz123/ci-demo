3aba62156798d4d308d823e463937a6862fbd172
Lingzi <susanzhang0521@gmail.com>
2023-03-22 18:20:11 -0400

    commit

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 const { v4: uuidv4 } = require('uuid');
 
-console.log('Hello World: ' + uuidv4())
-console.log("My name is Susan and I like boba :D")
+console.log('Hello World: ' + uuidv4());
+console.log('My name is Susan and I like boba :D');
